cee3b69fff5cb6422971ab87101ae1457c9458b4
Hash-himadri <hashcodes324@gmail.com>
2024-01-17 00:15:19 +0530

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:time_zone/Pages/Loading.dart';
 import 'package:time_zone/Pages/choose_location.dart';
 import 'Pages/home.dart';
 
-
+//Trying github
 void main() {
   runApp(MaterialApp(
     home: ChooseLocation(),
